36bfab6f8f894301fde9b544a2468a637dde1115
Owen Jow <owenjow@berkeley.edu>
2019-06-02 23:55:55 -0700

    exps with diff noise types

---
+++ experiment03.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# experiment03
+# ------------
+# Reproduces results from section 5.1 in Vincent et al.
+# Trains a single-layer denoising autoencoder on natural image patches.
+# This experiment uses salt-and-pepper noise (instead of additive Gaussian noise).
+
+python3 sdae.py \
+    --batch_size 128 \
+    --learning_rate 0.006 \
+    --num_epochs 100 \
+    --model_key olshausen_ae \
+    --dataset olshausen \
+    --olshausen_path /home/owen/workspace/sdae/data/natural/images.mat \
+    --olshausen_step_size 6 \
+    --noise_type sp \
+    --sp_frac 0.25
---
+++ experiment04.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# experiment04
+# ------------
+# Reproduces results from section 5.1 in Vincent et al.
+# Trains a single-layer denoising autoencoder on natural image patches.
+# This experiment uses zero-masking noise (as opposed to Gaussian/S&P noise).
+
+python3 sdae.py \
+    --batch_size 128 \
+    --learning_rate 0.004 \
+    --num_epochs 500 \
+    --model_key olshausen_ae \
+    --dataset olshausen \
+    --olshausen_path /home/owen/workspace/sdae/data/natural/images.mat \
+    --olshausen_step_size 6 \
+    --noise_type mn \
+    --zero_frac 0.65
