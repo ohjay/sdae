6961433d370c4c0986697906442918e872934a0c
Owen Jow <owenjow@berkeley.edu>
2019-06-04 02:27:13 -0700

    pretrain stacked regular AE too

---
+++ experiments/13.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# experiment13
+# ------------
+# Pretrains a stacked REGULAR autoencoder for future use.
+# The trained SAE should be saved in ./ckpt/stage1_sae.pth.
+
+python3 sdae.py \
+    --batch_size 128 \
+    --learning_rate 0.005 \
+    --num_epochs 100 \
+    --model_key mnist_sae2 \
+    --dataset mnist \
+    --noise_type n/a \
+    --weight_decay 0.0000001 \
+    --save_path ./ckpt/stage1_sae.pth
